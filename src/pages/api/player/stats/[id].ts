2e6792768675ee804d0903b661f58b280b13e483
Cornelius <cornelius.denninger@gmail.com>
2022-01-09 15:47:39 +0100

    fix: :ambulance: Updated drunk beer

--- src/pages/api/player/stats/[id].ts
+++ src/pages/api/player/stats/[id].ts
@@ -6,18 +6,23 @@ import { supabase } from '@/utils/client';
 
 export default async function hello(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
-  const { data } = await supabase.from('games_solo').select('*');
+  const { data } = await supabase
+    .from('games_solo')
+    .select('*')
+    .or(`player1.eq.${id},player2.eq.${id}`);
 
   let schickeln = 0;
   let played = 0;
   let won = 0;
+  let drunk = 0;
   data?.map((e) => {
     if (e.player1 == id || e.player2 == id) {
       played += 1;
     }
     if (e.winner == id) {
       won += 1;
-    }
+      drunk += (0.5 / 6) * (6 - e.cupsleft);
+    } else drunk += (0.5 / 6) * e.cupsleft + 0.5;
 
     if (e.schnickeln == id) {
       schickeln += 1;
@@ -31,7 +36,7 @@ export default async function hello(req: NextApiRequest, res: NextApiResponse) {
         won: won,
         schnickeln: schickeln,
         winrate: ((won / played) * 100).toFixed(2) ?? 0,
-        drunk: played * 0.5,
+        drunk: drunk.toFixed(2),
       },
     });
   } else {
