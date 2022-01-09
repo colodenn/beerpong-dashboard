ce3b8b4ca74529653698e2e9450c3a87fddca71d
Cornelius <cornelius.denninger@gmail.com>
2022-01-09 14:09:55 +0100

    fix: :rocket: Personal table own profile on one side

--- src/components/stats/TablePersonal.tsx
+++ src/components/stats/TablePersonal.tsx
@@ -27,49 +27,16 @@ export default function Table(props: { id: string }) {
                   <tr key={i} className=''>
                     <td>
                       <Link
-                        href={`/player/${e.player1.username}`}
-                        passHref={true}
-                      >
-                        <div className='flex items-center space-x-3 cursor-pointer'>
-                          <div className='flex items-center'>
-                            <div className='flex items-center mr-2 w-12 h-12'>
-                              {e.winner == e.player1.username && (
-                                <Image
-                                  alt=''
-                                  className='mx-auto my-auto mr-2'
-                                  height={25}
-                                  width={25}
-                                  src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/crown_1f451.png'
-                                />
-                              )}
-                            </div>
-                            <div className='avatar'>
-                              <div className='mask mask-squircle w-12 h-12'>
-                                <Image
-                                  height={55}
-                                  width={55}
-                                  src={e.player1.avatar_url}
-                                  alt='Avatar Tailwind CSS Component'
-                                />
-                              </div>
-                            </div>
-                          </div>
-                          <div>
-                            <div className='font-bold'>
-                              {e.player1.username}
-                            </div>
-                          </div>
-                        </div>
-                      </Link>
-                    </td>
-                    <td>
-                      <Link
-                        href={`/player/${e.player2.username}`}
+                        href={`/player/${
+                          e.player2.username == props.id
+                            ? e.player1.username
+                            : e.player2.username
+                        }`}
                         passHref={true}
                       >
                         <div className='flex items-center space-x-3 cursor-pointer'>
                           <div className='flex items-center mr-2 w-12 h-12'>
-                            {e.winner == e.player2.username && (
+                            {e.winner != props.id && (
                               <Image
                                 alt=''
                                 className='mx-auto my-auto mr-2'
@@ -84,14 +51,67 @@ export default function Table(props: { id: string }) {
                               <Image
                                 height={55}
                                 width={55}
-                                src={e.player2.avatar_url}
+                                src={
+                                  e.player2.username == props.id
+                                    ? e.player1.avatar_url
+                                    : e.player2.avatar_url
+                                }
                                 alt='Avatar Tailwind CSS Component'
                               />
                             </div>
                           </div>
                           <div>
                             <div className='font-bold'>
-                              {e.player2.username}
+                              {e.player2.username == props.id
+                                ? e.player1.username
+                                : e.player2.username}
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
+                    </td>
+                    <td>
+                      <Link
+                        href={`/player/${
+                          e.player1.username == props.id
+                            ? props.id
+                            : e.player2.username
+                        }`}
+                        passHref={true}
+                      >
+                        <div className='flex items-center space-x-3 cursor-pointer'>
+                          <div className='flex items-center'>
+                            <div className='flex items-center mr-2 w-12 h-12'>
+                              {e.winner == props.id && (
+                                <Image
+                                  alt=''
+                                  className='mx-auto my-auto mr-2'
+                                  height={25}
+                                  width={25}
+                                  src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/crown_1f451.png'
+                                />
+                              )}
+                            </div>
+                            <div className='avatar'>
+                              <div className='mask mask-squircle w-12 h-12'>
+                                <Image
+                                  height={55}
+                                  width={55}
+                                  src={
+                                    e.player1.username == props.id
+                                      ? e.player1.avatar_url
+                                      : e.player2.avatar_url
+                                  }
+                                  alt='Avatar Tailwind CSS Component'
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div>
+                            <div className='font-bold'>
+                              {e.player1.username == props.id
+                                ? props.id
+                                : e.player2.username}
                             </div>
                           </div>
                         </div>
