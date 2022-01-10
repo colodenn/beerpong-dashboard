094119c0478020c8ce9eae9d2ae37f8dfd129b94
Cornelius <cornelius.denninger@gmail.com>
2022-01-11 00:51:09 +0100

    fix: :ambulance: Exclude player unentschieden

--- src/pages/api/players/stats.ts
+++ src/pages/api/players/stats.ts
@@ -7,6 +7,7 @@ import { supabase } from '@/utils/client';
 export default async function hello(req: NextApiRequest, res: NextApiResponse) {
   const { data } = await supabase
     .from('profiles')
-    .select('username, avatar_url');
+    .select('username, avatar_url')
+    .neq('username', 'unentschieden');
   res.status(200).json({ players: data });
 }
