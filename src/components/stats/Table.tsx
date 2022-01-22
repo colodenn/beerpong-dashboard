4391cdf2664246e814b62c67f563c8c3f138d479
Cornelius <cornelius.denninger@gmail.com>
2022-01-22 12:31:36 +0100

    feat: :sparkles: Split into component template

--- src/components/stats/Table.tsx
+++ src/components/stats/Table.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Image from 'next/image';
-import Link from 'next/link';
 import { Key } from 'react';
 import useSWR from 'swr';
+
+import { Player } from './Player';
 
 const fetcher = (args: any) => fetch(args).then((res) => res.json());
 
@@ -26,76 +26,18 @@ export default function Table() {
                 return (
                   <tr key={i} className=''>
                     <td>
-                      <Link
-                        href={`/player/${e.player1.username}`}
-                        passHref={true}
-                      >
-                        <div className='flex items-center space-x-3 cursor-pointer'>
-                          <div className='flex items-center'>
-                            <div className='flex items-center mr-2 w-12 h-12'>
-                              {e.winner == e.player1.username && (
-                                <Image
-                                  alt=''
-                                  className='mx-auto my-auto mr-2'
-                                  height={25}
-                                  width={25}
-                                  src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/crown_1f451.png'
-                                />
-                              )}
-                            </div>
-                            <div className='avatar'>
-                              <div className='mask mask-squircle w-12 h-12'>
-                                <Image
-                                  height={55}
-                                  width={55}
-                                  src={e.player1.avatar_url}
-                                  alt='Avatar Tailwind CSS Component'
-                                />
-                              </div>
-                            </div>
-                          </div>
-                          <div>
-                            <div className='font-bold'>
-                              {e.player1.username}
-                            </div>
-                          </div>
-                        </div>
-                      </Link>
+                      <Player
+                        username={e.player1.username}
+                        winner={e.winner}
+                        avatar_url={e.player1.avatar_url}
+                      />
                     </td>
                     <td>
-                      <Link
-                        href={`/player/${e.player2.username}`}
-                        passHref={true}
-                      >
-                        <div className='flex items-center space-x-3 cursor-pointer'>
-                          <div className='flex items-center mr-2 w-12 h-12'>
-                            {e.winner == e.player2.username && (
-                              <Image
-                                alt=''
-                                className='mx-auto my-auto mr-2'
-                                height={25}
-                                width={25}
-                                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/crown_1f451.png'
-                              />
-                            )}
-                          </div>
-                          <div className='avatar'>
-                            <div className='mask mask-squircle w-12 h-12'>
-                              <Image
-                                height={55}
-                                width={55}
-                                src={e.player2.avatar_url}
-                                alt='Avatar Tailwind CSS Component'
-                              />
-                            </div>
-                          </div>
-                          <div>
-                            <div className='font-bold'>
-                              {e.player2.username}
-                            </div>
-                          </div>
-                        </div>
-                      </Link>
+                      <Player
+                        username={e.player2.username}
+                        winner={e.winner}
+                        avatar_url={e.player2.avatar_url}
+                      />
                     </td>
                     <td className=''>{e.cupsleft}</td>
                   </tr>
