1cdcbfb48e6a5ebd61eafc121fd65ece7f51985f
Nicolas Klein <nicolasklein1998@gmail.com>
2022-11-06 16:07:19 +0100

    fix(header): now correctly sizes on mobile

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -24,6 +24,7 @@ export default function Header() {
       label: e.name,
     };
   });
+
   const [season, setSeason] = useLocalStorageState('SS 22');
   useEffect(() => {
     setSeason(season ? season : 0);
@@ -33,43 +34,38 @@ export default function Header() {
   // function changeTheme() {
   //   setIsDarkMode(!isDarkMode);
   // }
-  return (
-    <header className='bg-primary border-b-3 fixed top-0 z-50 pb-4 mx-auto w-full bg-white bg-opacity-60 border-b-2 border-gray-200 backdrop-filter backdrop-blur-md backdrop-saturate-150'>
-      <div className='container grid grid-cols-3 auto-cols-max mx-auto mt-6 w-full'>
-        <div className='flex items-center'>
-          <Hamburger toggled={isOpen} toggle={setOpen} />
-          {isOpen && (
-            <ul className='fade-in slide-in-bottom ml-2 md:flex md:ml-8'>
-              <li className='underline-offset-2 ml-1 text-lg font-semibold underline cursor-pointer md:ml-4'>
-                {!user ? (
-                  <Link href='/login'>login</Link>
-                ) : (
-                  <span onClick={() => logoutUser()}>logout</span>
-                )}
-              </li>
-              {user && (
-                <li className='underline-offset-2 ml-1 text-lg font-semibold underline cursor-pointer md:ml-4'>
-                  <Link
-                    href={
-                      profile?.username
-                        ? `/player/${profile.username}`
-                        : '/profile'
-                    }
-                  >
-                    profile
-                  </Link>
-                </li>
-              )}
-              <li className='underline-offset-2 ml-1 text-lg font-semibold underline cursor-pointer md:ml-4'>
-                <Link href={'/players'}>players</Link>
-              </li>
-              <li className='underline-offset-2 ml-1 text-lg font-semibold underline cursor-pointer md:ml-4'>
-                <Link href={'/rules'}>rules</Link>
-              </li>
-              <li className='underline-offset-2 ml-1 text-lg font-semibold underline cursor-pointer md:ml-4'>
-                <Link href={'/badges'}>badges</Link>
-              </li>
-              {/* <li className='underline-offset-2 ml-4 text-lg font-semibold underline cursor-pointer'>
+
+  const getNavOptions = () => {
+    return (
+      <ul className='fade-in slide-in-bottom py-8 ml-2 space-y-4 text-4xl md:flex md:py-0 md:ml-8 md:space-y-0 md:text-lg'>
+        <li className='underline-offset-2 ml-1 font-semibold underline cursor-pointer md:ml-4'>
+          {!user ? (
+            <Link href='/login'>login</Link>
+          ) : (
+            <span onClick={() => logoutUser()}>logout</span>
+          )}
+        </li>
+        {user && (
+          <li className='underline-offset-2 ml-1 font-semibold underline cursor-pointer md:ml-4'>
+            <Link
+              href={
+                profile?.username ? `/player/${profile.username}` : '/profile'
+              }
+            >
+              profile
+            </Link>
+          </li>
+        )}
+        <li className='underline-offset-2 ml-1 font-semibold underline cursor-pointer md:ml-4'>
+          <Link href={'/players'}>players</Link>
+        </li>
+        <li className='underline-offset-2 ml-1 font-semibold underline cursor-pointer md:ml-4'>
+          <Link href={'/rules'}>rules</Link>
+        </li>
+        <li className='underline-offset-2 ml-1 font-semibold underline cursor-pointer md:ml-4'>
+          <Link href={'/badges'}>badges</Link>
+        </li>
+        {/* <li className='underline-offset-2 ml-4 text-lg font-semibold underline cursor-pointer'>
                 table
               </li>
               <li className='underline-offset-2 ml-4 text-lg font-semibold underline cursor-pointer'>
@@ -78,39 +74,48 @@ export default function Header() {
               <li className='underline-offset-2 ml-4 text-lg font-semibold underline cursor-pointer'>
                 rules
               </li> */}
-            </ul>
-          )}
-        </div>
-        <div className='flex justify-center items-center text-xl font-bold'>
-          <Link passHref={true} href='/'>
-            <div className='flex justify-center items-center text-xl font-bold'>
-              <div className='mr-2 cursor-pointer'>
-                <Image
-                  alt='logo'
-                  width={48}
-                  height={48}
-                  className='mx-atuo my-auto'
-                  src='/images/redcuplogo.png'
-                />
-              </div>
-            </div>
-          </Link>
-        </div>
-        <div className='flex justify-end items-center'>
-          <Select
-            className='basic-single max-w-fit'
-            isSearchable={false}
-            isClearable={false}
-            classNamePrefix='select'
-            options={seasonDropdownOptions}
-            value={seasonDropdownOptions?.filter((option: any) => {
-              return option.value == season;
-            })}
-            onChange={(e) => setSeason(e.value)}
-          ></Select>
-        </div>
+      </ul>
+    );
+  };
 
-        {/* <div
+  return (
+    <div>
+      <header className='bg-primary border-b-3 fixed top-0 z-50 px-8 pb-4 mx-auto w-full bg-white bg-opacity-60 border-b-2 border-gray-200 backdrop-filter backdrop-blur-md backdrop-saturate-150'>
+        <div className='container grid grid-cols-3 auto-cols-max mx-auto mt-6 w-full'>
+          <div className='flex items-center'>
+            <Hamburger toggled={isOpen} toggle={setOpen} />
+            <div className='hidden md:block'>{isOpen && getNavOptions()}</div>
+          </div>
+          <div className='flex justify-center items-center text-xl font-bold'>
+            <Link passHref={true} href='/'>
+              <div className='flex justify-center items-center text-xl font-bold'>
+                <div className='mr-2 cursor-pointer'>
+                  <Image
+                    alt='logo'
+                    width={48}
+                    height={48}
+                    className='mx-atuo my-auto'
+                    src='/images/redcuplogo.png'
+                  />
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className='flex justify-end items-center'>
+            <Select
+              className='basic-single max-w-fit'
+              isSearchable={false}
+              isClearable={false}
+              classNamePrefix='select'
+              options={seasonDropdownOptions}
+              value={seasonDropdownOptions?.filter((option: any) => {
+                return option.value == season;
+              })}
+              onChange={(e) => setSeason(e.value)}
+            ></Select>
+          </div>
+
+          {/* <div
           data-toggle-theme='dark,light'
           className='flex items-center justify-end'
         >
@@ -121,7 +126,15 @@ export default function Header() {
             size={55}
           />
         </div> */}
-      </div>
-    </header>
+        </div>
+        <div
+          className={`flex justify-center text-xl md:hidden ${
+            isOpen ? 'block' : 'hidden'
+          }`}
+        >
+          {getNavOptions()}
+        </div>
+      </header>
+    </div>
   );
 }
