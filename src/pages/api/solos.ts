3698fe47a4bf50af5be60f71b95f4674c3301884
Cornelius <cornelius.denninger@gmail.com>
2022-11-06 22:22:30 +0100

    fix(types): :rocket: added type support for api - just need to add trpc for frontend typesafety

--- src/pages/api/solos.ts
+++ src/pages/api/solos.ts
@@ -2,13 +2,16 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 import { supabase } from '@/utils/client';
 
+import { Database } from '@/types/supabase';
+
 export default async function hello(req: NextApiRequest, res: NextApiResponse) {
   const { data } = await supabase
-    .from('games_solo')
+    .from<Database['public']['Tables']['games_solo']['Row']>('games_solo')
     .select(
       'cupsleft, winner,player1:profiles!player1 (avatar_url,username), player2:profiles!player2 (avatar_url,username), timestamp'
     )
     .order('timestamp', { ascending: false })
     .limit(20);
+
   res.send({ games: data });
 }
